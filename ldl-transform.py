55eefb2cdfa7dfd32e2697df2d4624db1b84b5c0
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-10-22 11:12:20 -0500

    Update ldl-transform.py

--- ldl-transform.py
+++ ldl-transform.py
@@ -1,270 +1,168 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 import glob
-from os import listdir , sep, path
 import os
 import argparse
-from dateutil import parser
 
 def process_command_line_arguments():
-    parser = argparse.ArgumentParser(description='Post Processing Process For LDL Content Migration Using Islandora Workbench')
-    parser.add_argument('-c', '--csv_directory', type=str, help='Path to metadata', required=False)
-    parser.add_argument('-f', '--files_directory', type=str, help='Path to the files', required=False)
-    parser.add_argument('-o', '--output_directory', type=str, help='Path to the output csv containing paths, frequency, and error reports', required=False)
-    args = parser.parse_args()
-    return args
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(
+        description='Post Processing for LDL Content Migration Using Islandora Workbench')
+    parser.add_argument('-c', '--csv_directory', type=str, help='Path to metadata CSV', required=True)
+    parser.add_argument('-f', '--files_directory', type=str, help='Path to RDF files', required=True)
+    parser.add_argument('-o', '--output_directory', type=str, help='Path for output CSV', required=True)
+    return parser.parse_args()
 
-################### 1) Getting data and fill the file column if files exist in the Data directory ########################
-def input_directory(csvs, OBJS):
-    LDLdf = pd.DataFrame(pd.read_csv(csvs,encoding='utf-8'))
-    LDLdf.rename(columns= {'PID' : 'id'},  inplace = True)
-    coll_name = []
-    coll_num = []
-    obj_string = []
-    id_to_list = LDLdf["id"].tolist() ###Putting the elements of id column to a list###
-    for IDs in id_to_list:
-        splitted_IDs= IDs.split(':')
-        coll_name.append(splitted_IDs[0])
-        coll_num.append(splitted_IDs[1])
-    for colls in range(len(coll_name)):
-        obj_string.append("{}_{}_OBJ".format(coll_name[colls], coll_num[colls]))
-    # From directory we put the file names and their type to a dictionary istead of using multiple lists then will compare to the list of customized file names with _OBJ from file_name to see if the file in directory did not have obj retur empty
-    file_dict = {}
-    for file in os.listdir(OBJS):
-        if "OBJ" in file:
-            name, ext = os.path.splitext(file)
-            file_dict[name] = ext
-    #Filling the file_column list to fill the file column:
-    file_column = []
-    for each in obj_string:
-        if each in file_dict:
-            file_column.append("Data/{}{}".format(each, file_dict[each]))
-        else:
-            file_column.append("")
+def load_metadata(csv_file):
+    """Load metadata from CSV and return a DataFrame."""
+    metadata_df = pd.read_csv(csv_file, encoding='utf-8')
+    metadata_df.rename(columns={'PID': 'id'}, inplace=True)
+    return metadata_df
 
-    #id slice of the 
-    keys = ['amistad', 'apl', 'cppl', 'dcc', 'ebrpl', 'fpoc', 'hicks', 'hnoc', 'lasc', 'lsm', 'lsu', 'lsua', 'lsus', 'lsuhsc', 'lsuhscs', 'latech', 'loyno', 'louisiananewspapers', 'mcneese', 'nojh', 'nicholls', 'nsu', 'oplib', 'slu','subr', 'sowela','tahil']
-    values = ['Amistad', 'APL', 'CPPL', 'DCC', 'EBRPL', 'FPOC', 'Hicks', 'HNOC', 'LASC', 'LSM', 'LSU', 'LSUA', 'LSUS', 'LSUHSC', 'LSUSHCS', 'LATECH', 'LouisianaNewspapers', 'LOYNO', 'McNeese', 'NOJH', 'Nicholls', 'NSU', 'OPLIB', 'SLU', 'SUBR', 'SOWELA', 'TAHIL']
+def update_file_column(metadata_df, files_directory):
+    """Update the 'file' column in the metadata DataFrame with file paths."""
+    obj_names = ["{}_{}_OBJ".format(id.split(':')[0], id.split(':')[1])
+                 for id in metadata_df["id"].tolist()]
+
+    # Create a dictionary for file name lookup
+    file_dict = {os.path.splitext(file)[0]: os.path.splitext(file)[1] 
+                 for file in os.listdir(files_directory) if "OBJ" in file}
+
+    metadata_df["file"] = [
+        f"Data/{obj_name}{file_dict.get(obj_name, '')}" if obj_name in file_dict else ""
+        for obj_name in obj_names
+    ]
+
+    return metadata_df
+
+def map_access_terms(metadata_df):
+    """Map collection keys to group names."""
+    keys = ['amistad', 'apl', 'cppl', 'dcc', 'ebrpl', 'fpoc', 'hicks', 'hnoc',
+            'lasc', 'lsm', 'lsu', 'lsua', 'lsus', 'lsuhsc', 'lsuhscs', 'latech',
+            'loyno', 'louisiananewspapers', 'mcneese', 'nojh', 'nicholls', 'nsu',
+            'oplib', 'slu', 'subr', 'sowela', 'tahil']
     
-    map_terms = {}
+    values = ['Amistad', 'APL', 'CPPL', 'DCC', 'EBRPL', 'FPOC', 'Hicks', 'HNOC',
+              'LASC', 'LSM', 'LSU', 'LSUA', 'LSUS', 'LSUHSC', 'LSUSHCS', 'LATECH',
+              'LouisianaNewspapers', 'LOYNO', 'McNeese', 'NOJH', 'Nicholls', 'NSU',
+              'OPLIB', 'SLU', 'SUBR', 'SOWELA', 'TAHIL']
 
-    for k in range(len(keys)):
-        map_terms[keys[k]] = values[k]
+    term_mapping = dict(zip(keys, values))
+    metadata_df["field_access_terms"] = metadata_df["id"].apply(lambda x: term_mapping.get(x.split('-')[0], ''))
+    
+    return metadata_df
 
+def parse_rdf_files(rdf_directory):
+    """Parse RDF files and extract relevant metadata."""
+    rdf_files = glob.glob(f"{rdf_directory}/*.rdf")
+    rdf_files.sort()
 
-    field_access_terms = []
-    for i in id_to_list:
-        field_access_terms.append(map_terms[i.split('-')[0]])
+    tags = []
+    attributes = []
+    text_values = []
+    
+    for rdf_file in rdf_files:
+        try:
+            tree = ET.parse(rdf_file)
+            root = tree.getroot()
+            
+            for element in root.iter():
+                tags.append(element.tag)
+                attributes.append(element.attrib)
+                text_values.append(element.text)
+                
+        except ET.ParseError as e:
+            print(f"Error parsing {rdf_file}: {e}")
+    
+    return tags, attributes, text_values
 
-
-    LDLdf["file"] = file_column
-    LDLdf["parent_id"] = ""
-    LDLdf["field_weight"] = ""
-    LDLdf["field_member_of"] = ""
-    LDLdf["field_model"] = "" #The number of resource type according to collection, obj or any other kind in the resource types in drupal
-    LDLdf["field_access_terms"] = field_access_terms #customized field for groups, which is a number associated with the group names number
-    LDLdf["field_resource_type"] = "" #The number of resource type according to collection, obj or any other kind in the resource types in drupal
-    LDLdf.drop("field_date_captured", inplace=True ,axis= 1, errors='ignore')
-    LDLdf.drop("field_is_preceded_by", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("field_is_succeeded_by", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("field_form_URI", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("field_form_authURI", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("field_rights_statement", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("field_rights_statement_uri", inplace=True ,axis= 1,errors='ignore')
-    LDLdf.drop("nan", inplace=True ,axis= 1,errors='ignore')
-
-    #fill nul values
-    LDLdf = LDLdf.apply(lambda col: col.fillna(''))
-    return LDLdf
-
-
-#################### 2) fill field_member_of, parent_id, field_weight column ########################
-
-def input_RDF(RDF_dir, LDL):
-    data = glob.glob("{}/*.rdf".format(RDF_dir))
-    tags = [] #getting none-splitted
-    val = [] #adding values to
-    tag_name = [] #ALL the Tags in the rdf
-    attrib = []
-    text = []
-    text_list = []
+def process_rdf_metadata(tags, attributes, text_values):
+    """Extract and process data from RDF metadata."""
+    tag_names = [tag.split('}')[-1] for tag in tags]
+    
     weight_list = []
-    parent = []
-    date_issueds = []
-    file_type = []
     content_type = []
-    data.sort()
+    parent_ids = []
+    viewers = []
+    issue_dates = []
     
-    for dirs in data:
-        rdf = ET.parse("{}".format(dirs))
-        itter = rdf.iter()
-        for inner in itter:
-            tags.append(inner.tag)
-            val.append(inner.attrib)
-            text.append(inner.text)
-
-    for tag in tags:
-        split_tags = tag.split('}')
-        tag_name.append(split_tags[1]) # ALL THE TAGS
-    for vals in val:
-        attrib.append(list(vals.values()))
-    # loop through text to extract dateIssued text, if no text then append empty string
-    for snippet in text:
-        if snippet != None and "\n" not in snippet and snippet != 'true' and len(snippet) > 4:
-                text_list.append(snippet)
-        else:
-            text_list.append('')
-    for i in range(len(text_list)):
-        date_issueds.append(text_list[i])
-    for num in range(len(tags)):
-        name_tag = tags[num].split('}')
-        if "isSequenceNumberOf" in name_tag[1]:
-            weight_list.append(text[num])
+    for tag, attrib, text in zip(tag_names, attributes, text_values):
+        if "isSequenceNumberOf" in tag:
+            weight_list.append(text)
         else:
             weight_list.append("")
-    mylist = list(zip(tag_name, attrib, weight_list, date_issueds))
-
-    #loop through all tupels and group each item's tupels into a list
-    item_list = []
-    group_list = []
-    viewer = []
-    for tupel_item in mylist:
-        group_list.append(list(tupel_item))
-        if tupel_item[0] == 'RDF' and len(group_list) > 1:
-            item_list.append(group_list)
-            group_list = [list(tupel_item)]
-    if group_list:
-        item_list.append(group_list)
-    
-    for i in range(len(item_list)):
-        if item_list[i][2][1][0] == 'info:fedora/islandora:bookCModel':
-            content_type.append('Paged Content')
-            viewer.append('Mirador')
-        if item_list[i][2][1][0] == 'info:fedora/islandora:sp_large_image_cmodel':
-            content_type.append('Image')
-            viewer.append('OpenSeadragon')
-        if item_list[i][2][1][0]  == 'info:fedora/islandora:sp-audioCModel':
-            content_type.append('Audio')
-            viewer.append('')
-        if item_list[i][2][1][0]  == 'info:fedora/islandora:sp_videoCModel':
-            content_type.append('Video')
-            viewer.append('')
-        if item_list[i][3][0] != 'deferDerivatives':
-            if item_list[i][3][1][0] == 'info:fedora/islandora:collectionCModel':
-                content_type.append('Collection')
-                viewer.append('')
-        if item_list[i][2][1][0] == 'info:fedora/islandora:newspaperCModel':
-            content_type.append('Newspaper')
-            viewer.append('')
-        if item_list[i][2][1][0] == 'info:fedora/islandora:newspaperIssueCModel':
-            content_type.append('Publication Issue')
-            viewer.append('')
-        if item_list[i][2][1][0] == 'info:fedora/islandora:sp_pdf':
-            content_type.append('Document')
-            viewer.append('PDF.js')
-        if item_list[i][2][1][0] == 'info:fedora/islandora:compoundCModel':
-            content_type.append('Compound Object')
-            viewer.append('')
-        if item_list[i][3][0]  == 'hasModel':
-            if item_list[i][3][1][0] == 'info:fedora/islandora:bookCModel':
-                content_type.append('Paged Content')
-                viewer.append('Mirador')
-            if item_list[i][3][1][0] == 'info:fedora/islandora:sp_large_image_cmodel':
-                content_type.append('Image')
-                viewer.append('OpenSeadragon')
-            if item_list[i][3][1][0]  == 'info:fedora/islandora:sp-audioCModel':
-                content_type.append('Audio')
-                viewer.append('')
-            if item_list[i][3][1][0]  == 'info:fedora/islandora:sp_videoCModel':
-                content_type.append('Video')
-                viewer.append('')
-            if item_list[i][3][1][0] == 'info:fedora/islandora:newspaperCModel':
-                content_type.append('Newspaper')
-                viewer.append('')
-            if item_list[i][3][1][0] == 'info:fedora/islandora:newspaperIssueCModel':
-                content_type.append('Publication Issue')
-                viewer.append('')
-            if item_list[i][3][1][0] == 'info:fedora/islandora:sp_pdf':
-                content_type.append('Document')
-                viewer.append('PDF.js')
-            if item_list[i][3][1][0] == 'info:fedora/islandora:compoundCModel':
-                content_type.append('Compound Object')
-                viewer.append('')
         
-    weight = []
-    field_member_of = []
-    count = []
-
-    #modified this loop to get isMemberOf value for each issue's parent_id
-    for item in item_list:
-        if item[3][0] == 'isMemberOf':
-            parent_pid = item[3][1][0].split("/")
-            parent.append(parent_pid[1])
-            weight.append('') 
-        if item[2][0] == 'isMemberOfCollection' and item[2][1][0].split('/')[1] == 'islandora:root':
-            # parent.append(item[2][1][0].split('/')[1])
-            parent.append('')
-            weight.append('')
-        if item[2][0] == 'isMemberOfCollection' and item[2][1][0].split('/')[1] != 'islandora:root':
-            parent.append(item[2][1][0].split('/')[1])
-            # parent.append('')
-            weight.append('')
-        if item[3][0] == 'isMemberOfCollection':
-            parent.append(item[3][1][0].split('/')[1])
-            # parent.append('')
-            weight.append('')
-        if item[3][0] == 'isConstituentOf':
-            parent.append(item[3][1][0].split('/')[1])
-            weight.append(item[4][2])
-        if item[3][0] == 'deferDerivatives':
-            parent.append(item[4][1][0].split('/')[1])        
-            weight.append('')
-
-    issue_dates = []
-    for r in range(len(item_list)):
-        if r+1 > (len(item_list)):
-            break
+        # Determine content type and viewer based on model
+        model = attrib.get('MODEL', '')
+        if model == 'info:fedora/islandora:bookCModel':
+            content_type.append('Paged Content')
+            viewers.append('Mirador')
+        elif model == 'info:fedora/islandora:sp_large_image_cmodel':
+            content_type.append('Image')
+            viewers.append('OpenSeadragon')
+        elif model == 'info:fedora/islandora:sp_pdf':
+            content_type.append('Document')
+            viewers.append('PDF.js')
         else:
-            if "isSequenceNumberOf" in item_list[r][0]:
-                collectionName = RDF_dir.split("/")[6]
-                nameofnumber = item_list[r][0]
-                ParentNumber = nameofnumber.split("_")[1]
-                # parent.append("{}:{}".format(collectionName, ParentNumber))
-                # weight.append(item_list[r][2])
-            if "dateIssued" == item_list[r][-3][0]:
-                issue_dates.append(item_list[r][-3][3])
-            else:
-                issue_dates.append("")
-                    
-    LDL["parent_id"] = parent    
-    LDL["field_weight"] = weight
-    LDL["field_model"] = content_type
-    LDL["field_viewer_override"] = viewer
-    LDL["field_edtf_date_issued"] = issue_dates
-    LDL["field_edtf_date_created"] = ""
-    LDL["field_linked_agent"] = ""
-    LDL.sort_values(by='field_model', ascending=True, inplace=True)
-    LDL.sort_values(by='field_identifier', ascending=False, inplace=True)
-    LDL.sort_values(by='parent_id', ascending=True, inplace=True)
-    LDL.sort_values(by='field_weight', ascending=True, inplace=True)
+            content_type.append('')
+            viewers.append('')
 
+        # Extract date issued
+        if "dateIssued" in tag and text:
+            issue_dates.append(text)
+        else:
+            issue_dates.append('')
+        
+        # Extract parent ID
+        if "isMemberOf" in tag and text:
+            parent_ids.append(text.split('/')[-1])
+        else:
+            parent_ids.append('')
+    
+    return weight_list, content_type, viewers, parent_ids, issue_dates
 
-    return LDL
+def update_metadata_with_rdf(metadata_df, rdf_directory):
+    """Update metadata DataFrame with data from RDF files."""
+    tags, attributes, text_values = parse_rdf_files(rdf_directory)
+    
+    weight_list, content_type, viewers, parent_ids, issue_dates = process_rdf_metadata(tags, attributes, text_values)
+    
+    metadata_df["parent_id"] = parent_ids
+    metadata_df["field_weight"] = weight_list
+    metadata_df["field_model"] = content_type
+    metadata_df["field_viewer_override"] = viewers
+    metadata_df["field_edtf_date_issued"] = issue_dates
+    metadata_df["field_edtf_date_created"] = ""
+    metadata_df["field_linked_agent"] = ""
 
-def write(input_df, output_filename):
-    Workbench_ready_csv = input_df.to_csv("{}".format(output_filename), index=False)
-    print('*******************************************\nData post processed and written to csv ...\n*******************************************')
-    return Workbench_ready_csv
+    # Sorting for better organization
+    metadata_df.sort_values(by=['field_model', 'field_identifier', 'parent_id', 'field_weight'], 
+                            ascending=[True, False, True, True], inplace=True)
+    
+    return metadata_df
 
+def write_output(metadata_df, output_file):
+    """Write the final processed metadata to a CSV file."""
+    metadata_df.to_csv(output_file, index=False)
+    print(f"Data successfully written to {output_file}")
 
 def main():
     args = process_command_line_arguments()
-    LDLdf_1 = input_directory(args.csv_directory,args.files_directory)
-    input_file = input_RDF(args.files_directory,LDLdf_1)
-    output = write(input_file,args.output_directory)
-main()
 
+    # Step 1: Load metadata from CSV
+    metadata_df = load_metadata(args.csv_directory)
 
+    # Step 2: Update file column based on available OBJ files
+    metadata_df = update_file_column(metadata_df, args.files_directory)
 
-## Command:
-# To process one file at a time >>> python3 LDL-post-processing.py -c LDL_Tracking_Spreadsheet/amistad-pgoudvis/xml2csv_amistad-pgoudvis.csv -f LDL_Tracking_Spreadsheet/amistad-pgoudvis/Data -o LDL_Tracking_Spreadsheet/amistad-pgoudvis
+    # Step 3: Map access terms
+    metadata_df = map_access_terms(metadata_df)
+
+    # Step 4: Update metadata with RDF data
+    metadata_df = update_metadata_with_rdf(metadata_df, args.files_directory)
+
+    # Step 5: Write the final CSV output
+    write_output(metadata_df, args.output_directory)
+
+if __name__ == "__main__":
+    main()
